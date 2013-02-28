3e8f58388ae4481895be298530a4b1b613f43dd1
Hari Caushik <hcaushik@gmail.com>
2013-02-28 10:52:37 -0800

    marked off all even numbers

--- primes.c
+++ primes.c
@@ -16,6 +16,8 @@ struct compFinder {
 	int threadNo;
 };
 
+void printBitMap(char * nlist, char mask[8], int start, int end);
+
 int main(int argc, char *argv[])
 {
 	char *nlist;		/* list of numbers up to max value */
@@ -36,6 +38,7 @@ int main(int argc, char *argv[])
 	
 	/* mark the number 1 as special */
 	nlist[1] = nlist[1] | mask[1];
+	nlist[4] = nlist[4] | mask[4];
 	if (nlist[1] & mask[1])
 		printf("Number 1 has been marked as special.\n");
 	for (i = 0; i < 8; i++)
@@ -43,6 +46,34 @@ int main(int argc, char *argv[])
 		printf("%d ", (nlist[i] & mask[i]) > 0);
 	}
 	printf("\n");
+
+	/* mark all even numbers */
+	for (i = 4; i < BYTES; i += 2)
+	{
+		int byte;
+		int bit;
+
+		byte = i / 8;
+		bit = i % 8;
+		nlist[byte] = nlist[byte] | mask[bit];
+	}
+	printBitMap(nlist, mask, 1, 15);
 	free(nlist);
 	return 0;
+}
+
+void printBitMap(char *nlist, char mask[8],  int start, int end)
+{
+	int i;
+	for (i = start; i <= end; i++)
+	{
+		int byte;
+		int bit;
+
+		byte = i / 8;
+		bit = i % 8;
+		printf("%d ", (nlist[byte] & mask[bit]) > 0);
+	}
+	printf("\n");
+
 }
